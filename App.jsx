04639a064998106a456074b4d244be8be13ab591
Ritista Shrestha <ritista99@gmail.com>
2024-06-25 22:02:49 +0545

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+    {/* sm, md, xl, 2xl */}
+      <h1 className="text-[100px] md:underline bg-red-500 text-black mx-6 my-6">Hello </h1>
+      <h2 className="text-3xl md:container bg-red-700 mx-auto text-center pt-3 pb-3 rounded-2xl">Hello </h2>
+      <h3 className="text-2xl">Hello </h3>
+      <h4 className="text-xl">Hello </h4> 
+      <p>hello </p>
+      </>
+  )
+}
+
+export default App
